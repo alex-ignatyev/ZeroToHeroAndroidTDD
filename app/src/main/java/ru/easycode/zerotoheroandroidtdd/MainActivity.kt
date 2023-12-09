936c44fe9e1a651f05ce4f7207e5154e1b90ee3b
tanya <swishtan@mail.ru>
2023-12-09 13:57:15 +0300

    task/010 Task010Test solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -12,23 +12,31 @@ class MainActivity : AppCompatActivity() {
     private lateinit var textView: TextView
     private lateinit var button: Button
     private var removeTextView = false
+    private var enabledButton = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (savedInstanceState != null) {
-            if (savedInstanceState.getBoolean(KEY)) {
-                rootLayout.removeView(textView)
-                removeTextView = true
-            }
-        }
-
         rootLayout = findViewById(R.id.rootLayout)
         textView = findViewById(R.id.titleTextView)
         button = findViewById(R.id.removeButton)
 
+        if (savedInstanceState != null) {
+            if (savedInstanceState.getBoolean(TEXTVIEW_KEY)) {
+                rootLayout.removeView(textView)
+                removeTextView = true
+            }
+
+            val isButtonEnabled = savedInstanceState.getBoolean(BUTTON_KEY)
+            button.isEnabled = isButtonEnabled
+            enabledButton = isButtonEnabled
+        }
+
+
         button.setOnClickListener {
+            button.isEnabled = false
+            enabledButton = false
             rootLayout.removeView(textView)
             removeTextView = true
         }
@@ -36,10 +44,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putBoolean(KEY, removeTextView)
+        outState.putBoolean(TEXTVIEW_KEY, removeTextView)
+        outState.putBoolean(BUTTON_KEY, enabledButton)
     }
 
     private companion object {
-        const val KEY = "key"
+        const val TEXTVIEW_KEY = "textView_key"
+        const val BUTTON_KEY = "button_key"
     }
 }
