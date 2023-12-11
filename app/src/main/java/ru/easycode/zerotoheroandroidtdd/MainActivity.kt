abbeca873a3114ab5e8e7d85f9e113f8d22fd10c
tanya <swishtan@mail.ru>
2023-12-11 13:59:32 +0300

    task/012 Task012Test solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var textView: TextView
     private lateinit var button: Button
     private val countBase = Count.Base(2, 4)
-    private lateinit var uiState: UiState
+    private var uiState: UiState? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,14 +20,17 @@ class MainActivity : AppCompatActivity() {
         textView = findViewById(R.id.countTextView)
         button = findViewById(R.id.incrementButton)
 
-        if (savedInstanceState != 0){
-
-
+        if (savedInstanceState != null) {
+            val save = savedInstanceState.getParcelable<UiState>(KEY_UISTATE) ?: return
+            uiState = save
+            textView.text = save.text
+            val buttonBlocked = save is UiState.Base
+            button.isEnabled = buttonBlocked
         }
 
         button.setOnClickListener {
             uiState = countBase.increment(textView.text.toString())
-            textView.text = uiState.text
+            textView.text = uiState?.text ?: ""
             val buttonBlocked = uiState is UiState.Base
             button.isEnabled = buttonBlocked
         }
