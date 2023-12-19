5e99c4694aa3e2a0c045df05e70124542f7cdca9
tanya <swishtan@mail.ru>
2023-12-19 15:09:34 +0300

    task/014 Task014Test solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,35 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var progressBar: ProgressBar
+    private lateinit var textView: TextView
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        progressBar = findViewById(R.id.progressBar)
+        textView = findViewById(R.id.titleTextView)
+        button = findViewById(R.id.actionButton)
+
+        button.setOnClickListener {
+            button.isEnabled = false
+            progressBar.visibility = View.VISIBLE
+            button.postDelayed({
+                textView.visibility = View.VISIBLE
+                progressBar.visibility = View.GONE
+                button.isEnabled = true
+            }, 3500)
+        }
+
     }
 }
---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/UiState.kt
@@ -0,0 +1,18 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import kotlinx.parcelize.Parcelize
+
+interface UiState {
+/*
+    @Parcelize
+    class Loading() : Parcelize, UiState:
+    {
+
+
+    }
+
+    @Parcelize
+    class Success() : Parcelize, UiState {
+
+    }*/
+}
