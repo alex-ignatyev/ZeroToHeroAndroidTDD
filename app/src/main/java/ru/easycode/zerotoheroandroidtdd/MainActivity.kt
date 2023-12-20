811ef62377be89c4c795924f39f094650606ac6e
tanya <swishtan@mail.ru>
2023-12-20 17:11:43 +0300

    task/015 Task015Test solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,39 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import ru.easycode.zerotoheroandroidtdd.UiState.ShowData
+import ru.easycode.zerotoheroandroidtdd.UiState.ShowProgress
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var progressBar: ProgressBar
+    private lateinit var textView: TextView
+    private lateinit var button: Button
+    private val repository = Repository.Base()
+    private val liveDataWrapper = LiveDataWrapper.Base()
+    private val vm = MainViewModel(liveDataWrapper, repository)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        progressBar = findViewById(R.id.progressBar)
+        textView = findViewById(R.id.titleTextView)
+        button = findViewById(R.id.actionButton)
+
+        liveDataWrapper.liveData().observe(this) { uiState ->
+            progressBar.isVisible = uiState is ShowProgress
+            button.isEnabled = uiState !is ShowProgress
+            textView.isVisible = uiState is ShowData
+        }
+
+        button.setOnClickListener {
+            vm.load()
+        }
     }
 }
---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/UiState.kt
@@ -0,0 +1,8 @@
+package ru.easycode.zerotoheroandroidtdd
+
+interface UiState {
+
+    object ShowProgress: UiState
+
+    object ShowData: UiState
+}
